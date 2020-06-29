4e7fdf8b9d5487f2be972cd1e94b03bfb47d8922
Mookyung <pporri@yonsei.ac.kr>
2020-06-29 10:57:34 +0900

    Initial Commit

---
+++ NavigationViewTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationViewTest
+//
+//  Created by Mookyung Kwak on 2020/06/29.
+//  Copyright © 2020 Mookyung Kwak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
